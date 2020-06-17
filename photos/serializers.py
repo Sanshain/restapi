cba2ab6bd4afee8ad64f812321dc5dd26350396c
Sanshain <digital-mag@ya.ru>
2020-06-17 21:57:17 +0300

    api for Article creation

--- photos/serializers.py
+++ photos/serializers.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from .models import Article
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -22,3 +22,7 @@ class ArticleSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=120)
     description = serializers.CharField()
     body = serializers.CharField()
+    author_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
