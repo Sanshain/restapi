8c03e8626cb03666bc58ece8976fb74f3463d318
Sanshain <digital-mag@ya.ru>
2020-06-18 11:45:47 +0300

    deleted method added

--- photos/views.py
+++ photos/views.py
@@ -62,3 +62,12 @@ class ArticleView(APIView):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
 
+
+    def delete(self, request, pk):
+
+        print '///////////'
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({
+            "message": "Article with id `{}` has been deleted.".format(pk)
+        }, status=204)
